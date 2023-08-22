a00f6517d9495f2e73af106a72d0f22b74d0c991
dsong124 <142925982+dsong124@users.noreply.github.com>
2023-08-22 12:59:41 -0400

    Play2048.java

---
+++ Play2048.java
@@ -0,0 +1,27 @@
+//Daniel Song
+//Program Description
+//May 18, 2021
+
+public class Play2048
+{
+   public static void main(String[] args) 
+   {
+      Board2048 gameBoard = new Board2048();
+    
+      gameBoard.showBoard();
+      System.out.println("Left");
+      gameBoard.moveLeft();  
+      gameBoard.showBoard();
+      System.out.println("Right");
+      gameBoard.moveRight();
+      gameBoard.showBoard();
+      System.out.println("Down");
+      System.out.println(gameBoard.canMoveDown());     
+      gameBoard.moveDown();
+      gameBoard.showBoard();
+      System.out.println(gameBoard.canMoveDown());     
+      
+      
+      
+   }
+}
